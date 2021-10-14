8b468f6e32f8891a95a11fd99d1c9453803c611f
Mohamed-Sanim300 <73278692+Mohamed-Sanim300@users.noreply.github.com>
2021-10-14 11:47:29 +0100

    Update model.py

--- SPD Siamese/model.py
+++ SPD Siamese/model.py
@@ -5,9 +5,19 @@ class Net(nn.Module):
         # self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
-        x = self.trans1(x)
-        x = self.rect1(x)
         x = self.tangent(x)
-        # x = self.dropout(x)
         x = self.linear(x.type(torch.FloatTensor))
         return x
+
+class SiameseNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(SiameseNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        return output1, output2
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
