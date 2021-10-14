7f86b1b5265df8d77209b575fe48ea6fb739c162
Mohamed-Sanim300 <73278692+Mohamed-Sanim300@users.noreply.github.com>
2021-10-14 11:01:55 +0100

    Update dataset.py

--- DHG-14/dataset.py
+++ DHG-14/dataset.py
@@ -1,8 +1,3 @@
-import tarfile
-!wget http://www-rech.telecom-lille.fr/shrec2017-hand/HandGestureDataset_SHREC2017.tar.gz
-tar = tarfile.open("/content/HandGestureDataset_SHREC2017.tar.gz", "r:gz")
-tar.extractall("/content/")
-
 
 class Shreck(Dataset):
 
